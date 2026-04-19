6b32f5f700cf5670df7be4aba0f2344050d06187
Rbj <hrakotoarivelo75@gmail.com>
2026-04-19 23:05:51 +0200

    Henintsoa 19/04/2026 navbar modifié et acceuil commencé

--- home.php
+++ home.php
@@ -6,6 +6,7 @@
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="home.css">
 </head>
 
 <header>
@@ -13,10 +14,16 @@
 </header>
 
 <body>
-    <div class="text-center m-3 p-3 bg-secondary-subtle rounded-5" id="">
-        <div class="d-inline" style="background-color: #e313ff70;">
-            <p>Informatique & Tech</p>
+    <div class="text-center m-3 p-4 rounded-5" id="desc">
+        <div class="d-inline-block px-5 rounded-5" style="background-color: #EADDFF; height: 30px;">
+            <h5>Informatique & Tech</h5>
         </div>
+        <br><br>
+        <h3>Teste tes connaissances </h3>
+        <br>
+        <p>Des questions sur le web, les bases de données, les algorithmes et plus encore.</p>
+        <br>
+        <button type="submit" class="btn px-5 rounded-4">Commencer le quiz</button>
     </div>
 </body>
 
