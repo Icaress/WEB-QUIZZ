39b10c2f4a78580a369c0e0c474f6b02bc723a2a
Icaress <malarsicaru9@gmail.com>
2026-04-24 17:50:16 +0200

    Leon 24/04/2026 règle terminer + avancement de admin panel

--- Admin/Admin_panel.php
+++ Admin/Admin_panel.php
@@ -4,9 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="Admin_panel.css">
     <title>Panneau Admin</title>
 </head>
+<header>
+    <div class="d-flex align-items-center justify-content-between p-3">
+        <div class="d-flex align-items-center">
+            <img src="../Image/logo_site_WEB_QUIZZ.png" alt="" width="90" height="90" class="rounded">
+            <p class="fw-bold fst-italic fs-1 mb-0">WEB QUIZZ</p>
+        </div>
+    </div>
+</header>
 <body>
     <div class="info">
         <div> <p class="big">240</p> <p>questions</p> </div>
