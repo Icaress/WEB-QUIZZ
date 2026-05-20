4e9fa19cf9276be47bda4b810ef7505e543f6bfd
Rbj <hrakotoarivelo75@gmail.com>
2026-05-20 15:50:48 +0200

    Modifier_timer dans admin modifié

--- Admin/Modifier_timer.php
+++ Admin/Modifier_timer.php
@@ -26,16 +26,22 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="Modifier_timer.css">
 </head>
 <body>
 
-    <div><p>The current cooldown is : <?= $hr.":".$min.":".$sec ?></p></div>
-    
-    <form action="" method="post">
-        <label for="cooldown">Modify : </label>
-        <input type="time" name="cooldown" step="1" id="cooldown">
-        <button type="submit">Confirm</button>
-    </form>
+    <div class="d-flex flex-column align-items-center justify-content-center">
+        <h1 class="fst-italic"><strong>Modifier la durée d'une tentative</strong></h1>
+        <p>Durée d'une tentative : <?= $hr.":".$min.":".$sec ?></p>
+        
+        <form action="" method="post">
+            <label for="cooldown">Modifier : </label>
+            <input type="time" name="cooldown" step="1" id="cooldown">
+            <button type="submit">Confirm</button>
+        </form>
+    </div>
 
 </body>
 </html>
